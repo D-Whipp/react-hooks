7ea3e7e438c041c4626d3743a4a863ffbc72cefd
David Whipple <dwhipp88@gmail.com>
2022-11-10 16:58:26 -0700

    changed document.title with useEffect

--- src/index.js
+++ src/index.js
@@ -1,36 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { FaStar } from 'react-icons/fa';
-
-const createArray = (length) => [...Array(length)];
-
-function Star({ selected = false, onSelect }) {
-  return (
-    <FaStar color={selected ? 'red' : 'grey'} onClick={onSelect} />
-  );
-}
-
-function StarRating({ totalStars = 5 }) {
-  const [selectedStars, setSelectedStars] = useState(0);
-  return (
-    <>
-      {createArray(totalStars).map((n, i) => (
-        <Star
-          key={i}
-          selected={selectedStars > i}
-          onSelect={() => setSelectedStars(i + 1)}
-        />
-      ))}
-      <p>
-        {selectedStars} of {totalStars}
-      </p>
-    </>
-  );
-}
 
 function App() {
-  return <StarRating totalStars={5} />;
+  const [name, setName] = useState('Nic');
+  useEffect(() => {
+    document.title = `Celebrate ${name}`;
+  });
+  return (
+    <section>
+      <p>Congratulations {name}!</p>
+      <button onClick={() => setName('Nicci')}>Change Winner</button>
+    </section>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
