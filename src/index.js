74767d14f2a00eb27bab4310d4de44e311b15e03
David Whipple <dwhipp88@gmail.com>
2022-11-10 15:26:08 -0700

    created component tree

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { FaStar } from 'react-icons/fa';
+
+const createArray = (length) => [...Array(length)];
+
+function Star({ selected = false }) {
+  return <FaStar color={selected ? 'red' : 'grey'} />;
+}
+
+function StarRating({ totalStars = 5 }) {
+  return createArray(totalStars).map((n, i) => <Star key={i} />);
+}
 
 function App() {
-  const [status, setStatus] = useState('not listening');
-  return (
-    <div>
-      <h1>I'm {status}.</h1>
-      <button onClick={() => setStatus('listening')}>listen</button>
-    </div>
-  );
+  return <StarRating totalStars={10} />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
