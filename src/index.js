6f2093e97e5da55e2cd35aa674f09086e5004985
David Whipple <dwhipp88@gmail.com>
2022-11-10 14:55:16 -0700

    created useState branch, used a checkbox to look at useState

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function App() {
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <div>
+      <h1>I'm listening</h1>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={() => setChecked((checked)=> !checked)}
+      ></input>
+      <p>{checked ? 'checked' : 'not checked'}</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +24,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
