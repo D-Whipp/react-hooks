a082a291576781cb75133c3c82022252789474c2
David Whipple <dwhipp88@gmail.com>
2022-11-10 15:33:44 -0700

    used a star rating system to send interactions up component tree

--- src/index.js
+++ src/index.js
@@ -5,16 +5,32 @@ import { FaStar } from 'react-icons/fa';
 
 const createArray = (length) => [...Array(length)];
 
-function Star({ selected = false }) {
-  return <FaStar color={selected ? 'red' : 'grey'} />;
+function Star({ selected = false, onSelect }) {
+  return (
+    <FaStar color={selected ? 'red' : 'grey'} onClick={onSelect} />
+  );
 }
 
 function StarRating({ totalStars = 5 }) {
-  return createArray(totalStars).map((n, i) => <Star key={i} />);
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars}
+      </p>
+    </>
+  );
 }
 
 function App() {
-  return <StarRating totalStars={10} />;
+  return <StarRating totalStars={5} />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
