d2ad92456bddc75c4fff7e08a530a19b1b146d83
David Whipple <dwhipp88@gmail.com>
2022-11-11 15:18:44 -0700

    refactored code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from "./App"
+import App from './App';
 
-export const TreesContext = createContext();
+const TreesContext = createContext();
+
+export const useTrees = () => useContext(TreesContext);
 
 const trees = [
-{id: "1", type: "Maple"},
-{id: "2", type: "Spruce"},
-{id: "3", type: "Willow"},
-{id: "4", type: "Whipple"},
-]
-
-// you can handle form inputs with
-// controlled components
-// means you handle form inputs using
-// state variables
-
+  { id: '1', type: 'Maple' },
+  { id: '2', type: 'Spruce' },
+  { id: '3', type: 'Willow' },
+  { id: '4', type: 'Whipple' },
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <TreesContext.Provider value={{trees}}>
-  <App />
+  <TreesContext.Provider value={{ trees }}>
+    <App />
   </TreesContext.Provider>
 );
-
