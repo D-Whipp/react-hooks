a115d353a37be565ba9ba12724b6714b286ace9a
David Whipple <dwhipp88@gmail.com>
2022-11-10 17:18:37 -0700

    specified which useEffect fired with dependency array

--- src/index.js
+++ src/index.js
@@ -4,20 +4,23 @@ import './index.css';
 
 function App() {
   const [name, setName] = useState('Nic');
+  const [admin, setAdmin] = useState(false);
   useEffect(() => {
-    document.title = `Celebrate ${name}`;
-  });
+    console.log(`Celebrate ${name}`);
+  }, [name]);
+
+  useEffect(() => {
+    console.log(`The user is: ${admin ? 'admin' : 'not admin'}`);
+  }, [admin]);
   return (
     <section>
       <p>Congratulations {name}!</p>
       <button onClick={() => setName('Nicci')}>Change Winner</button>
+      <p>{admin ? 'logged in' : 'not logged in'}</p>
+      <button onClick={() => setAdmin(true)}>Log In</button>
     </section>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
