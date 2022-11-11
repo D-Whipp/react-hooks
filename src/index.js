11408b1b1e1343e273ba7d1a0f48bcc75c9cd2dd
David Whipple <dwhipp88@gmail.com>
2022-11-11 12:44:00 -0700

    captured form input values with conrolled component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+// you can handle form inputs with
+// controlled components
+// means you handle form inputs using
+// state variables
 function App() {
-  const sound = useRef();
-  const color = useRef();
+  const [sound, setSound] = useState('');
+  const [color, setColor] = useState('#000');
+  // const sound = useRef();
+  // const color = useRef();
 
   const submit = (e) => {
     e.preventDefault();
-    const soundVal = sound.current.value;
-    const colorVal = color.current.value;
-    alert(`${soundVal} sounds like ${colorVal}`);
-    sound.current.value = '';
-    color.current.value = '';
+    // const soundVal = sound.current.value;
+    // const colorVal = color.current.value;
+    alert(`${sound} sounds like ${color}`);
+    setSound('');
+    setColor('#000');
+    // sound.current.value = '';
+    // color.current.value = '';
   };
 
   return (
     <form onSubmit={submit}>
-      <input ref={sound} type="text" placeholder="Sound..." />
-      <input ref={color} type="color" />
+      <input
+        value={sound}
+        onChange={(e) => setSound(e.target.value)}
+        type="text"
+        placeholder="Sound..."
+      />
+      <input
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+        type="color"
+      />
       <button>ADD</button>
     </form>
   );
