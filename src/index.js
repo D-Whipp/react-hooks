87cc72d837b47b4104a7a6d310f0bb0d8e89ac04
David Whipple <dwhipp88@gmail.com>
2022-11-11 14:32:14 -0700

    extracted logic into custom hook

--- src/index.js
+++ src/index.js
@@ -1,41 +1,27 @@
 import React, { useState, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { useInput } from './useInput';
 
 // you can handle form inputs with
 // controlled components
 // means you handle form inputs using
 // state variables
 function App() {
-  const [sound, setSound] = useState('');
-  const [color, setColor] = useState('#000');
-  // const sound = useRef();
-  // const color = useRef();
+  const [titleProps, resetTitle] = useInput('');
+  const [colorProps, resetColor] = useInput('#000');
 
   const submit = (e) => {
     e.preventDefault();
-    // const soundVal = sound.current.value;
-    // const colorVal = color.current.value;
-    alert(`${sound} sounds like ${color}`);
-    setSound('');
-    setColor('#000');
-    // sound.current.value = '';
-    // color.current.value = '';
+    alert(`${titleProps.value} sounds like ${colorProps.value}`);
+    resetTitle('');
+    resetColor('#000');
   };
 
   return (
     <form onSubmit={submit}>
-      <input
-        value={sound}
-        onChange={(e) => setSound(e.target.value)}
-        type="text"
-        placeholder="Sound..."
-      />
-      <input
-        value={color}
-        onChange={(e) => setColor(e.target.value)}
-        type="color"
-      />
+      <input {...titleProps} type="text" placeholder="Sound..." />
+      <input {...colorProps} type="color" />
       <button>ADD</button>
     </form>
   );
