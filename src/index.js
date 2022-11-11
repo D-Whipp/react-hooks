90aded4aaafafae3c03a44c5f0d5b852aa1a0d0f
David Whipple <dwhipp88@gmail.com>
2022-11-11 15:16:11 -0700

    retrieved data using useContext

--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
-import React, { useState, useRef } from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { useInput } from './useInput';
+import App from "./App"
+
+export const TreesContext = createContext();
+
+const trees = [
+{id: "1", type: "Maple"},
+{id: "2", type: "Spruce"},
+{id: "3", type: "Willow"},
+{id: "4", type: "Whipple"},
+]
 
 // you can handle form inputs with
 // controlled components
 // means you handle form inputs using
 // state variables
-function App() {
-  const [titleProps, resetTitle] = useInput('');
-  const [colorProps, resetColor] = useInput('#000');
 
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${titleProps.value} sounds like ${colorProps.value}`);
-    resetTitle('');
-    resetColor('#000');
-  };
-
-  return (
-    <form onSubmit={submit}>
-      <input {...titleProps} type="text" placeholder="Sound..." />
-      <input {...colorProps} type="color" />
-      <button>ADD</button>
-    </form>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <TreesContext.Provider value={{trees}}>
+  <App />
+  </TreesContext.Provider>
+);
+
